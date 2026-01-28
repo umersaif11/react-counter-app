219e6778716795314735c1e201c9eb804b4b35a9
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-01-28 18:01:20 +0500

    Attach the event listeners to buttons, value is increasing and decreasing in console but not updating the UI

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -4,14 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+  let counter = 15;
+  const addValue = () => {
+    counter ++;
+    console.log(counter);
+  }
+  const removeValue = () => {
+    counter --;
+    console.log(counter);
+  }
   return (
     <>
       <h1>Counter React App</h1>
-      <h2>Counter Value: 5</h2>
-      <button>Increase Value</button>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}>Increase Value</button>
       <br />
-      <button>Decrease Value</button>
+      <button
+      onClick={removeValue}>Decrease Value</button>
     </>
   )
 }
